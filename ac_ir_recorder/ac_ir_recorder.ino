6233bb8df09c35f716efb5715c21df15e4be413b
Eric Dalquist <eric.dalquist@gmail.com>
2015-07-03 21:12:40 -0700

    Add logic to decode NEC signals into HEX strings

--- ac_ir_recorder/ac_ir_recorder.ino
+++ ac_ir_recorder/ac_ir_recorder.ino
@@ -26,6 +26,20 @@ Signal Out   ->  Digital Pin 2
 (If using a 3V Arduino, you may connect V+ to +3V)
 */
 
+// From https://github.com/shirriff/Arduino-IRremote/blob/master/IRremoteInt.h
+#define NEC_BITS 32
+#define NEC_HDR_MARK	9000
+#define NEC_HDR_SPACE	4500
+#define NEC_BIT_MARK	560
+#define NEC_ONE_SPACE	1690
+#define NEC_ZERO_SPACE	560
+#define NEC_RPT_SPACE	2250
+#define MARK_EXCESS 100
+
+#define TOLERANCE 25  // percent tolerance in measurements
+#define LTOL (1.0 - TOLERANCE/100.)
+#define UTOL (1.0 + TOLERANCE/100.)
+
 #define LEDPIN A0
 #define IRPIN D0
 //you may increase this value on Arduinos with greater than 2k SRAM
@@ -33,6 +47,70 @@ Signal Out   ->  Digital Pin 2
 
 volatile  unsigned int irBuffer[maxLen]; //stores timings - volatile because changed by ISR
 volatile unsigned int x = 0; //Pointer thru irBuffer - volatile because changed by ISR
+
+int MATCH_MARK(int measured_us, int desired_us) {
+  return measured_us >= (desired_us - MARK_EXCESS) && measured_us <= (desired_us + MARK_EXCESS);
+}
+
+int MATCH_SPACE(int measured_us, int desired_us) {
+  return measured_us >= (desired_us - MARK_EXCESS) && measured_us <= (desired_us + MARK_EXCESS);
+}
+
+// NECs have a repeat only 4 items long
+long decodeNEC(int *irData) {
+  long data = 0;
+  int offset = 0;
+  // Initial mark
+  if (!MATCH_MARK(irData[offset], NEC_HDR_MARK)) {
+    Serial.println("ERR No NEC Header");
+    return -1;
+  }
+  offset++;
+  // Check for repeat
+  if (x == 4 &&
+    MATCH_SPACE(irData[offset], NEC_RPT_SPACE) &&
+    MATCH_MARK(irData[offset+1], NEC_BIT_MARK)) {
+    /*results->bits = 0;
+    results->value = REPEAT;
+    results->decode_type = NEC;*/
+    Serial.println("NEC Repeat");
+    return data;
+  }
+  if (x < 2 * NEC_BITS + 4) {
+    Serial.println("ERR not enough data");
+    return -1;
+  }
+  // Initial space
+  if (!MATCH_SPACE(irData[offset], NEC_HDR_SPACE)) {
+    Serial.println("ERR No NEC Header");
+    return -1;
+  }
+  offset++;
+  for (int i = 0; i < NEC_BITS; i++) {
+    if (!MATCH_MARK(irData[offset], NEC_BIT_MARK)) {
+      Serial.println("ERR Expected a Mark");
+      return -1;
+    }
+    offset++;
+    if (MATCH_SPACE(irData[offset], NEC_ONE_SPACE)) {
+      data = (data << 1) | 1;
+    }
+    else if (MATCH_SPACE(irData[offset], NEC_ZERO_SPACE)) {
+      data <<= 1;
+    }
+    else {
+      Serial.println("ERR Expected a Space");
+      return -1;
+    }
+    offset++;
+  }
+  // Success
+  /*results->bits = NEC_BITS; */ //32 bits
+  return data;
+  /*results->decode_type = NEC;*/
+  /*return DECODED;*/
+}
+
 
 void setup() {
   Serial.begin(115200); //change BAUD rate as required
@@ -47,20 +125,24 @@ void loop() {
   Serial.println(F("Press the button on the remote now - once only"));
   delay(5000); // pause 5 secs
   if (x) { //if a signal is captured
-    digitalWrite(LEDPIN, HIGH);//visual indicator that signal received
-    Serial.println();
-    /*Serial.print(F("[")); //dump raw header format - for library*/
-    /*Serial.print((x - 1));
-    Serial.println(F(", "));*/
     detachInterrupt(IRPIN);//stop interrupts & capture until finshed here
+    digitalWrite(LEDPIN, HIGH);//visual indicator that signal received
+
+    int irData[x];
+    Serial.println();
     for (int i = 1; i < x; i++) { //now dump the times
-      /*if (!(i & 0x1)) Serial.print(F("-"));*/
-      Serial.print(irBuffer[i] - irBuffer[i - 1]);
+      int timing = irBuffer[i] - irBuffer[i - 1];
+      irData[i-1] = timing;
+      Serial.print(irData[i-1]);
       if (i + 1 < x) Serial.print(F("\t"));
     }
-    x = 0;
-    /*Serial.println(F("]"));*/
+
     Serial.println();
+    long necCode = decodeNEC(irData);
+    Serial.print(F("NEC: "));
+    Serial.println(necCode, HEX);
+
+    x = 0;
     Serial.println();
     digitalWrite(LEDPIN, LOW);//end of visual indicator, for this time
     attachInterrupt(IRPIN, rxIR_Interrupt_Handler, CHANGE);//re-enable ISR for receiving IR signal
