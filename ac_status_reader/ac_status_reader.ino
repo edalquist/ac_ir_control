8cbd2995fe8614bb09eca68a548146b7e1a71365
Eric Dalquist <eric.dalquist@gmail.com>
2015-07-27 21:36:46 -0700

    MUCH more efficient ISR thanks to ScruffR on the particle.io forums!

--- ac_status_reader/ac_status_reader.ino
+++ ac_status_reader/ac_status_reader.ino
@@ -2,82 +2,74 @@
 #define CLOCK_PIN D0
 #define INPUT_PIN D1
 
-// I think the state is sent 6 bytes with each byte lasting ~2.886ms and then repeating.
-// This should give us 10 copies of the pattern
-#define MAX_LEN 60
-#define CYCLE_MAX 500 // max time in micros the AC controller spends pushing data into the register
+// The shift register sees 6 bytes repeatedly
+#define STATUS_LEN 6
+// Keep the last 6 status codes
+#define CAPTURE_COUNT 6
+#define BUFFER_LEN 6 * 6
+#define UPDATE_TIME_MAX 500 // max time in micros the AC controller spends pushing data into the register
 
-volatile bool firstCycle = true;
-volatile unsigned int cycleStart = 0;
-
-volatile unsigned int registerCount = 0;
-volatile unsigned int shiftRegBuffer[MAX_LEN];
+unsigned int cycleStart = 0;
+volatile uint8_t byteBuffer[BUFFER_LEN];
+volatile uint8_t* currentByte = byteBuffer;
 
 void setup() {
   Serial.begin(115200); //change BAUD rate as required
   pinMode(CLOCK_PIN, INPUT);
   pinMode(INPUT_PIN, INPUT);
   attachInterrupt(CLOCK_PIN, clock_Interrupt_Handler, RISING); //set up ISR for receiving shift register clock signal
+
 }
 
 void loop() {
-  noInterrupts();
-  if (firstCycle) {
-    Serial.println("Waiting on first cycle");
-    Serial.println(cycleStart);
-    Serial.println(cycleStart);
-  } else {
-    // Dump the input data
-    for (int i = 0; i < MAX_LEN; i++) {
-      Serial.print(shiftRegBuffer[i], HEX);
-      Serial.print(" ");
-    }
-    Serial.println();
+  uint8_t readBuffer[BUFFER_LEN];
 
-    firstCycle = true;
-    cycleStart = 0;
-    registerCount = 0;
-  }
+  // Copy the volatile byteBuffer to a local buffer to minimize the time that interrupts are off
+  noInterrupts();
+  memcpy(readBuffer, (const uint8_t*) byteBuffer, BUFFER_LEN);
   interrupts();
+
+  // TODO read the array and keep the 6 bytes that happen most frequently
+
+  // Dump the input data
+  for (int i = 0; i < BUFFER_LEN - 6; i++) {
+    Serial.print(readBuffer[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.println();
+
 
   delay(2000); // pause 2s
 }
 
 void clock_Interrupt_Handler() {
-  int input = digitalRead(INPUT_PIN);
-
   // Track the start of each cycle
   // zero out the shiftReg to start accumulating data
   int now = micros();
-  int cycleDiff = now - cycleStart;
-  if (cycleStart == 0 || cycleDiff > CYCLE_MAX) {
+  if (cycleStart == 0 || (now - cycleStart) > UPDATE_TIME_MAX) {
+    if (cycleStart != 0) {
+      // New cycle means go to the next byte
+      currentByte++;
+
+      // Roll over to the start of the buffer once BUFFER_LEN is reached
+      if (currentByte == (byteBuffer + BUFFER_LEN)) {
+        currentByte = byteBuffer;
+      }
+    }
+
+    // Record the update cycle start time for the current byte
     cycleStart = now;
 
-    registerCount++;
-
-    // Roll over to the start of the buffer once MAX_LEN is reached
-    if (registerCount == MAX_LEN) {
-      registerCount = 0;
-    }
-
-    shiftRegBuffer[registerCount] = 0;
+    // Zero the current byte, some cycles don't push a full 8 bits into the register
+    *currentByte = 0;
   }
 
-  // Skip the first cycle data on the off chance that we started listing during a register update
-  if (firstCycle) {
-    if (cycleDiff <= CYCLE_MAX) {
-      return;
-    } else {
-      firstCycle = false;
-    }
-  }
+  // On clock rise shift the data in the register by one
+  *currentByte <<= 1;
 
-  // On clock rise shift the data in the shift register by one
-  shiftRegBuffer[registerCount] = shiftRegBuffer[registerCount] << 1;
-  shiftRegBuffer[registerCount] &= 0x000000FF;
-
-  // If the input is high set the new bit to 1
-  if (input == HIGH) {
-    shiftRegBuffer[registerCount] |= 1;
+  // If the input is high set the new bit to 1 otherwise leave it zero
+  // faster way of reading pin D1: https://community.particle.io/t/reading-digits-from-dual-7-segment-10-pin-display/12989/6
+  if (((GPIOB->IDR) & 0b01000000) == 0b01000000) {
+    *currentByte |= 1;
   }
 }
